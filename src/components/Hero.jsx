4c544ab64833f823e26e37c78b291438a9cc1b95
akitindi <abdullkitindi21@gmail.com>
2025-07-27 12:44:45 +0300

    updated hero image url path

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="relative h-[90vh] w-full  bg-[url(./images/sunset.jpg)] bg-cover bg-center bg-no-repeat">
+    <div className="relative h-[90vh] w-full  bg-[url(/images/sunset.jpg)] bg-cover bg-center bg-no-repeat">
       <div className="relative w-full max-w-6xl mx-auto flex items-center  h-full">
         <div className="absolute left-0 top-32 z-20 mt-28">
           <h1 className="text-5xl w-full max-w-3xl text-white font-semibold mt-5 leading-tight">
