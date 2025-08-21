73b79316edb07e05e5bf4cb6813ddbc75e56f5ae
akitindi <abdullkitindi21@gmail.com>
2025-08-21 17:48:00 +0300

    added scrolling to th etop helper function

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import ScrollToTop from "./ScrollToTop";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    <ScrollToTop />
     <StrictMode>
       <App />
     </StrictMode>
